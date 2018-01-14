8a7bb7c7b891b0d808d862cdede836f0996c7c0e
bobcheng <bobchengzy@gmail.com>
2018-01-15 01:27:36 +0800

    finish some htmls

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,15 +2,13 @@
 namespace app\index\controller;
 
 use think\Controller;
-use think\Db;
-use think\db\exception\DataNotFoundException;
-use think\db\exception\ModelNotFoundException;
-use think\exception\DbException;
+
 
 class Index extends Controller
 {
     public function index()
     {
-        dump('ok');
+        //dump('ok');
+        return $this->fetch('main');
     }
 }
